e561378b0da67e0cf2911b319f25869e5c42d963
Rodrigo <rodex97@gmail.com>
2024-09-17 19:40:51 -0300

    Teste de carrinho+Teste de checkout

--- src/test/java/org/users/test/problem/problemUserTest.java
+++ src/test/java/org/users/test/problem/problemUserTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.users.test.ConfigLoader;
 
 public class problemUserTest {
@@ -39,8 +40,10 @@ public class problemUserTest {
             addBtn.click();
             navegador.findElement(By.xpath("//*[@id=\"remove-sauce-labs-backpack\"]")).click();
             Assertions.assertFalse(addBtn.isDisplayed());
-        // navegador.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
     }
+    // consigo adicionar um item ao carrinho = Adicionar um item ao carrinho e tentar remover
+    // mas não consigo tirar, pela pagina dos produtos
+
     @Test
     @DisplayName("Teste de imagem")
     public void imagens(){
@@ -58,8 +61,44 @@ public class problemUserTest {
         ConfigLoader.reportErrors();
     }
     // imagens erradas = Testar por src, verificar se é o mesmo das outras paginas
-    // consigo adicionar um item ao carrinho = Adicionar um item ao carrinho e tentar remover
-    // mas não consigo tirar, pela pagina dos produtos
+
+    @Test
+    @DisplayName("Teste de carrinho")
+    public void remCarrinho(){
+        WebElement filtroTextOld = navegador.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div/span/span"));
+
+        WebElement filtro = navegador.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div/span/select"));
+        Select select = new Select(filtro);
+        select.selectByIndex(1);
+
+        WebElement filtroTextNew = navegador.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div/span/span"));
+        Assertions.assertNotEquals(filtroTextOld, filtroTextNew);
+    }
     // o filtro não funciona = Verificar se eles funcionam e comparar
+
+    @Test
+    @DisplayName("Teste de checkout")
+    public void checkout(){
+        navegador.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]")).click();
+        navegador.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+        navegador.findElement(By.xpath("//*[@id=\"checkout\"]")).click();
+
+        WebElement priNome = navegador.findElement(By.xpath("//*[@id=\"first-name\"]"));
+        priNome.sendKeys("Maria");
+        WebElement segNome = navegador.findElement(By.xpath("//*[@id=\"last-name\"]"));
+        segNome.sendKeys("Serra");
+        WebElement codPostal = navegador.findElement(By.xpath("//*[@id=\"postal-code\"]"));
+        codPostal.sendKeys("124567890");
+
+        String priNomeText = priNome.getText();
+        String segNomeText = segNome.getText();
+        String codPostalText = codPostal.getText();
+
+        Assertions.assertEquals("Maria",priNomeText);
+        Assertions.assertEquals("Serra", segNomeText);
+        Assertions.assertEquals("124567890",codPostalText);
+
+        navegador.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+    }
     // checkout não me deixa digitar o LastName = Digitar e verificar se o valor do primeiro nome muda
 }
