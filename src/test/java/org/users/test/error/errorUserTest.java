b0124bb384cd40e15039f79785cd1b116759d263
Rodrigo <rodex97@gmail.com>
2024-09-19 18:10:28 -0300

    performanceUserTest+errorUserTest-começo

---
+++ src/test/java/org/users/test/error/errorUserTest.java
@@ -0,0 +1,9 @@
+package org.users.test.error;
+
+public class errorUserTest {
+    // não da para colocar sobrenome no cadastro de checkout
+    // não é possivel apertar o botão de finalizar
+    // não é possivel adicionar três
+    // filtro também não funciona
+
+}
---
+++ src/test/java/org/users/test/performance/performanceUserTest.java
@@ -0,0 +1,89 @@
+package org.users.test.performance;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.users.test.ConfigLoader;
+
+public class performanceUserTest {
+    private WebDriver navegador;
+
+    @BeforeEach
+    public void setup(){
+        ConfigLoader config = new ConfigLoader("src/test/resources/credentials");
+        navegador = ConfigLoader.inicializador("https://www.saucedemo.com/inventory.html");
+
+        WebElement userNamePath = navegador.findElement(By.xpath("//*[@id=\"user-name\"]"));
+        userNamePath.sendKeys(config.getUsername("performance"));
+
+        WebElement passwordPath = navegador.findElement(By.xpath("//*[@id=\"password\"]"));
+        passwordPath.sendKeys(config.getPassword());
+
+
+    }
+
+    @Test
+    @DisplayName("Teste de loading no login")
+    public void loadLogin(){
+        long stTime = System.currentTimeMillis();
+        WebElement loginBtn = navegador.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        loginBtn.click();
+        long enTime = System.currentTimeMillis();
+
+        long loadTime = enTime - stTime;
+        System.out.println("Demorou " + loadTime + " ms");
+
+        Assertions.assertTrue(loadTime < 2000, "Excedeu 2000ms");
+    }
+
+    @Test
+    @DisplayName("Teste de loading no filtro")
+    public void loadFil(){
+        WebElement loginBtn = navegador.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        loginBtn.click();
+
+        long stTime = System.currentTimeMillis();
+        WebElement filtro = navegador.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div/span/select"));
+        Select select = new Select(filtro);
+        select.selectByIndex(1);
+        long enTime = System.currentTimeMillis();
+
+        long loadTime = enTime - stTime;
+        System.out.println("Demorou " + loadTime + " ms");
+
+        Assertions.assertTrue(loadTime < 2000, "Excedeu 2000ms");
+    }
+
+    @Test
+    @DisplayName("Teste de loading checkout")
+    public void loadCheck(){
+        WebElement loginBtn = navegador.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        loginBtn.click();
+
+        navegador.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]")).click();
+        navegador.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+        navegador.findElement(By.xpath("//*[@id=\"checkout\"]")).click();
+
+        navegador.findElement(By.xpath("//*[@id=\"first-name\"]")).sendKeys("Maria");
+        navegador.findElement(By.xpath("//*[@id=\"last-name\"]")).sendKeys("Serra");
+        navegador.findElement(By.xpath("//*[@id=\"postal-code\"]")).sendKeys("124567890");
+        navegador.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+
+        navegador.findElement(By.xpath("//*[@id=\"finish\"]")).click();
+
+        long stTime = System.currentTimeMillis();
+        WebElement end = navegador.findElement(By.xpath("//*[@id=\"back-to-products\"]"));
+        end.click();
+        long enTime = System.currentTimeMillis();
+
+        long loadTime = enTime - stTime;
+        System.out.println("Demorou " + loadTime + " ms");
+
+        Assertions.assertTrue(loadTime < 2000, "Excedeu 2000ms");
+    }
+}
