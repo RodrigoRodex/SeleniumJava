e7df8096d9db660f519e2bae7acd1a959681b427
Rodrigo <rodex97@gmail.com>
2024-10-01 18:07:19 -0300

    LastNameTest+CardTest

--- src/test/java/org/users/test/error/errorUserTest.java
+++ src/test/java/org/users/test/error/errorUserTest.java
@@ -1,9 +1,68 @@
 package org.users.test.error;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.users.test.ConfigLoader;
+
 public class errorUserTest {
-    // não da para colocar sobrenome no cadastro de checkout
+    private WebDriver navegador;
+    private final String[] Card = {"-sauce-labs-backpack\"]",
+            "-sauce-labs-bike-light\"]",
+            "-sauce-labs-bolt-t-shirt\"]",
+            "-sauce-labs-fleece-jacket\"]",
+            "-sauce-labs-onesie\"]",
+            "-test.allthethings()-t-shirt-(red)\"]"};
+
+    @BeforeEach
+    public void setup(){
+        ConfigLoader config = new ConfigLoader("src/test/resources/credentials");
+        navegador = ConfigLoader.inicializador("https://www.saucedemo.com/inventory.html");
+
+        WebElement userNamePath = navegador.findElement(By.xpath("//*[@id=\"user-name\"]"));
+        userNamePath.sendKeys(config.getUsername("error"));
+
+        WebElement passwordPath = navegador.findElement(By.xpath("//*[@id=\"password\"]"));
+        passwordPath.sendKeys(config.getPassword());
+
+        WebElement loginBtn = navegador.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        loginBtn.click();
+    }
+
+    @Test
+    @DisplayName("Sobrenome no cadastro")
+    public void lastNameTest(){
+        // não da para colocar sobrenome no cadastro de checkout
+        navegador.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+        navegador.findElement(By.xpath("//*[@id=\"checkout\"]")).click();
+
+        WebElement fName = navegador.findElement(By.xpath("//*[@id=\"first-name\"]"));
+        fName.sendKeys("Maria");
+        Assertions.assertEquals("Maria",fName.getAttribute("value"));
+
+        WebElement lName = navegador.findElement(By.xpath("//*[@id=\"last-name\"]"));
+        lName.sendKeys("Serra");
+        Assertions.assertEquals("Serra", lName.getAttribute("value"));
+    }
+
     // não é possivel apertar o botão de finalizar
-    // não é possivel adicionar três
+    @Test
+    @DisplayName("Testando quais cards estão com problema")
+    public void cardTest(){
+        // não é possivel adicionar três
+        for (String s : Card) {
+            String cardAddBtnXPath = String.format("%s", "//*[@id=\"add-to-cart" + s);
+            String cardRemBtnXPath = String.format("%s", "//*[@id=\"remove" + s);
+
+            WebElement cardAddButton = navegador.findElement(By.xpath(cardAddBtnXPath));
+            cardAddButton.click();
+            navegador.findElement(By.xpath(cardRemBtnXPath));
+        }
+    }
     // filtro também não funciona
 
 }
